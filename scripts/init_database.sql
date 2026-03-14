4404d9e793ecc764eb3ca18ff0d8d3a178f3aa7f
Van Dung Pham <dungphamvan2709@gmail.com>
2026-03-14 19:20:34 +1000

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -2,6 +2,9 @@
 Create Databases and Schemas
 Check if existed database named 'DataWarehouse', then drop the database and recreate new database, else create database named 'DataWarehouse'
 Create 3 layer namely bronze, silver, gold schemas for database
+WARNING:
+Running the script will drop the existed database named 'DataWarehouse'. All data will be permanently deleted. Proceed with cautions and ensure to have backup 
+files properly ready before running the script.
 
 */
 
